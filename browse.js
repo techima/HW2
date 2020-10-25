6b84b75de7d5cf0a2a95f83a74152207208d1eaa
Patrik <patrik_pruunsild@hotmail.com>
2020-10-25 12:32:22 +0200

    follow button

--- browse.js
+++ browse.js
@@ -2,18 +2,10 @@ $(function(){
     $.get("https://private-anon-679a2b9640-wad20postit.apiary-mock.com/profiles", function(response){
       var i;
       for (i = 0; i < response.length; i++) {
+        let follow = $('<button type="button" class="followButton">').text("Follow");
         let picture = $('<img class="profilepic">').attr('src', response[i].avatar);
         let name = $('<p>').text(response[i].firstname + " " + response[i].lastname);
-        let square = null;
-        if (i == 0) {
-          square = $(".divSquare1").append(picture).append(name);
-        } else if (i == 1) {
-          square = $(".divSquare2").append(picture).append(name);
-        } else if (i == 2) {
-          square = $(".divSquare3").append(picture).append(name);
-        } else {
-          square = $(".divSquare4").append(picture).append(name);
-        }
+        square = $(".divSquare"+(i+1)).append(picture).append(name).append(follow);
         $(".main-container").append(square)
       }
     })
