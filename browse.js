f13797073e7f5f886de415f5d74812e8f40670ea
Patrik <patrik_pruunsild@hotmail.com>
2020-10-25 12:35:12 +0200

    Update browse.js

--- browse.js
+++ browse.js
@@ -3,6 +3,9 @@ $(function(){
       var i;
       for (i = 0; i < response.length; i++) {
         let follow = $('<button type="button" class="followButton">').text("Follow");
+        follow.click(function()) {
+          
+        }
         let picture = $('<img class="profilepic">').attr('src', response[i].avatar);
         let name = $('<p>').text(response[i].firstname + " " + response[i].lastname);
         square = $(".divSquare"+(i+1)).append(picture).append(name).append(follow);
