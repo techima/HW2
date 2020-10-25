e7fdccfa3a0151c6ff45ba2dca942dd93d49772a
Patrik <patrik_pruunsild@hotmail.com>
2020-10-25 19:43:26 +0200

    refactoring

--- browse.js
+++ browse.js
@@ -26,17 +26,4 @@ $(function(){
     }).fail(function(){
            alert("An error has occurred, cannot fetch profile data");
        });
-
-    $.get("https://private-anon-6630f0a8cc-wad20postit.apiary-mock.com/users/1", function(data){
-      let profile = $('.avatar').attr('src', data.avatar);
-      let myDropdown = $('#myDropdown');
-     	$("#myDropdown").hide();
-       $(".avatar").click(function(){
-         $("#myDropdown").toggle();
-       });
-       $(".name").text(data.firstname + " " + data.lastname);
-       $(".email").text(data.email);
-  }).fail(function(){
-         alert("An error has occurred, cannot fetch profile data");
-     });
   })
