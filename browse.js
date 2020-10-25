15b2ab920a72439b77a7a56c924060379bbf3952
Patrik <patrik_pruunsild@hotmail.com>
2020-10-25 12:17:04 +0200

    Browse page info

--- browse.js
+++ browse.js
@@ -1,21 +1,20 @@
 $(function(){
-$.get("https://private-anon-679a2b9640-wad20postit.apiary-mock.com/profiles", function(data){
-    $("#divSquare1").attr("src", data.avatar)
-}}));
-
-
-
-
-  function change(_this)
-{
-    if (_this.value=="Follow"){
-    _this.value = "Followed";
-    _this.style.backgroundColor = "Purple"
-    _this.style.color = "White"
-    }
-    else {
-    _this.value = "Follow";
-    _this.style.backgroundColor = "White"
-    _this.style.color = "Purple"
-    }
-}
+    $.get("https://private-anon-679a2b9640-wad20postit.apiary-mock.com/profiles", function(response){
+      var i;
+      for (i = 0; i < response.length; i++) {
+        let picture = $('<img class="profilepic">').attr('src', response[i].avatar);
+        let name = $('<p>').text(response[i].firstname + " " + response[i].lastname);
+        let square = null;
+        if (i == 0) {
+          square = $(".divSquare1").append(picture).append(name);
+        } else if (i == 1) {
+          square = $(".divSquare2").append(picture).append(name);
+        } else if (i == 2) {
+          square = $(".divSquare3").append(picture).append(name);
+        } else {
+          square = $(".divSquare4").append(picture).append(name);
+        }
+        $(".main-container").append(square)
+      }
+    })
+  })
