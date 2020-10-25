ea6d9a1cbfdc73397de64d94eb9269f7ff03570a
Brandon Loorits <looritsbrandon@gmail.com>
2020-10-25 18:21:03 +0200

    last

--- browse.js
+++ browse.js
@@ -21,7 +21,22 @@ $(function(){
         square = $(".divSquare"+(i+1)).append(picture).append(name).append(follow);
         $(".main-container").append(square)
       }
-    })
+    }).fail(function(){
+           alert("An error has occurred, cannot fetch profile data");
+       });
+
+    $.get("https://private-anon-6630f0a8cc-wad20postit.apiary-mock.com/users/1", function(data){
+      let profile = $('.avatar').attr('src', data.avatar);
+      let myDropdown = $('#myDropdown');
+     	$("#myDropdown").hide();
+       $(".avatar").click(function(){
+         $("#myDropdown").toggle();
+       });
+       $(".name").text(data.firstname + " " + data.lastname);
+       $(".email").text(data.email);
+  }).fail(function(){
+         alert("An error has occurred, cannot fetch profile data");
+     });
 
   })
 
