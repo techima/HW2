5a3af41875afaa83239c877a0158d69272eb61db
Tauno Tamm <57254922+taunotamm@users.noreply.github.com>
2020-10-25 16:53:13 +0200

    Button function in browse.js

--- browse.js
+++ browse.js
@@ -3,8 +3,18 @@ $(function(){
       var i;
       for (i = 0; i < response.length; i++) {
         let follow = $('<button type="button" class="followButton">').text("Follow");
-        follow.click(function(){
-
+        follow.click(function change()
+        {
+            if (this.innerHTML=="Follow"){
+            this.innerHTML = "Followed"
+            this.style.backgroundColor = "White"
+            this.style.color = "Purple"
+            }
+            else if(this.innerHTML = "Follwed"){
+            this.innerHTML = "Follow";
+            this.style.backgroundColor = "Purple"
+            this.style.color = "White"
+            }
         });
         let picture = $('<img class="profilepic">').attr('src', response[i].avatar);
         let name = $('<p>').text(response[i].firstname + " " + response[i].lastname);
@@ -13,3 +23,17 @@ $(function(){
       }
     })
   })
+
+  function change(_this)
+  {
+      if (_this.value=="Follow"){
+      _this.value = "Followed";
+      _this.style.backgroundColor = "Purple"
+      _this.style.color = "White"
+      }
+      else {
+      _this.value = "Follow";
+      _this.style.backgroundColor = "White"
+      _this.style.color = "Purple"
+      }
+  }
